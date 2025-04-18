6b49d7faf36c8e16646f8461b1a03601c234c3dc
Kevin-rm <kevinramarozatovo@gmail.com>
2025-04-18 21:10:52 +0300

    remove .idea folder

--- src/main/java/mg/matsd/FileWatcher.java
+++ src/main/java/mg/matsd/FileWatcher.java
@@ -1,4 +1,33 @@
 package mg.matsd;
 
-public class FileWatcher {
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.List;
+
+public class FileWatcher implements Runnable {
+    private final List<Path> paths;
+
+    public FileWatcher(List<Path> paths) {
+        this.paths = paths;
+    }
+
+    @Override
+    public void run() {
+        try (WatchService watchService = FileSystems.getDefault().newWatchService()) {
+            while (true) {
+                WatchKey watchKey = watchService.take();
+                watchKey.pollEvents().forEach(event -> {
+                    event.kind();
+                });
+
+                if (!watchKey.reset()) break;
+            }
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
