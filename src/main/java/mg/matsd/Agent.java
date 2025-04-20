d83f1a4b6af6fb588bd2abafbb305b9388f60e4e
Kevin-rm <kevinramarozatovo@gmail.com>
2025-04-20 17:11:56 +0300

    update pom

--- src/main/java/mg/matsd/Agent.java
+++ src/main/java/mg/matsd/Agent.java
@@ -5,6 +5,10 @@ import java.lang.instrument.Instrumentation;
 public class Agent {
     static Instrumentation instrumentation;
 
+    public static Instrumentation getInstrumentation() {
+        return instrumentation;
+    }
+
     public static void premain(String args, Instrumentation instrumentation) {
         Agent.instrumentation = instrumentation;
 
