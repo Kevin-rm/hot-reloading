3e2a975283a4b5602a5ddc72a225b73d4c208f69
Kevin-rm <kevinramarozatovo@gmail.com>
2025-04-25 10:58:18 +0300

    update agent and gitignore

--- src/main/java/mg/matsd/Agent.java
+++ src/main/java/mg/matsd/Agent.java
@@ -1,16 +1,24 @@
 package mg.matsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.lang.instrument.Instrumentation;
 
 public class Agent {
+    private static final Logger LOGGER = LogManager.getLogger(Agent.class);
 
     public static void premain(String args, Instrumentation instrumentation) {
-        DynamicClassLoader dynamicClassLoader = new DynamicClassLoader("/target/classes", instrumentation);
+        LOGGER.info("Démarrage de l'agent hot-reloading...");
+
+        DynamicClassLoader dynamicClassLoader = new DynamicClassLoader("target/classes", instrumentation);
         FileWatcher fileWatcher = new FileWatcher(dynamicClassLoader)
-            .addPath("/src/");
+            .addPath("src/");
 
         Thread thread = new Thread(fileWatcher, "FileWatcher-Thread");
         thread.setDaemon(true);
         thread.start();
+
+        LOGGER.info("Agent hot-reloading démarré avec succès");
     }
 }
